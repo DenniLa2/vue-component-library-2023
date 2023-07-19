3e332dd23de7831275d33b911b793866d137809c
Denis Abramyan <dennila2@ya.ru>
2023-07-19 05:06:09 +0300

    rename

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,10 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "BrianComponentLibrary",
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'UlukyUI',
       // the proper extensions will be added
-      fileName: "brian-component-lib",
+      fileName: 'uluky-ui',
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
