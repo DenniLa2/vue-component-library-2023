6e611df002e10ee8e9d0f1e3a8a348ed599f101b
Denis Abramyan <dennila2@ya.ru>
2023-07-24 12:54:03 +0300

    0.1.0-RC12 (icons)

--- dist/components/icon/24/index.d.ts
+++ dist/components/icon/24/index.d.ts
@@ -2,56 +2,56 @@
  * Created by Denis Abramyan (dennila2@gmail.com)
  * on 24.07.2023
  */
-import ICalendar from './Calendar.vue'
-import IChevron from './Chevron.vue'
-import ICircleEmpty from './CircleEmpty.vue'
-import ICircleError from './CircleError.vue'
-import ICircleSuccess from './CircleSuccess.vue'
-import IDownload from './Download.vue'
-import IExclamation from './Exclamation.vue'
-import IEyeClosed from './EyeClosed.vue'
-import IEyeOpened from './EyeOpened.vue'
-import IFile from './File.vue'
-import IFilter from './Filter.vue'
-import IFilterActive from './FilterActive.vue'
-import IHintSuccess from './HintSuccess.vue'
-import IInfo from './Info.vue'
-import IList from './List.vue'
-import IMail from './Mail.vue'
-import IMinus from './Minus.vue'
-import IPlus from './Plus.vue'
-import IPrinter from './Printer.vue'
-import IRefresh from './Refresh.vue'
-import ISearch from './Search.vue'
-import ISortArrow from './SortArrow.vue'
-import ITrash from './Trash.vue'
-import ITriplet from './Triplet.vue'
-import IX from './X.vue'
+import ICalendar24 from './Calendar.vue'
+import IChevron24 from './Chevron.vue'
+import ICircleEmpty24 from './CircleEmpty.vue'
+import ICircleError24 from './CircleError.vue'
+import ICircleSuccess24 from './CircleSuccess.vue'
+import IDownload24 from './Download.vue'
+import IExclamation24 from './Exclamation.vue'
+import IEyeClosed24 from './EyeClosed.vue'
+import IEyeOpened24 from './EyeOpened.vue'
+import IFile24 from './File.vue'
+import IFilter24 from './Filter.vue'
+import IFilterActive24 from './FilterActive.vue'
+import IHintSuccess24 from './HintSuccess.vue'
+import IInfo24 from './Info.vue'
+import IList24 from './List.vue'
+import IMail24 from './Mail.vue'
+import IMinus24 from './Minus.vue'
+import IPlus24 from './Plus.vue'
+import IPrinter24 from './Printer.vue'
+import IRefresh24 from './Refresh.vue'
+import ISearch24 from './Search.vue'
+import ISortArrow24 from './SortArrow.vue'
+import ITrash24 from './Trash.vue'
+import ITriplet24 from './Triplet.vue'
+import IX24 from './X.vue'
 
 export {
-  ICalendar,
-  IChevron,
-  ICircleEmpty,
-  ICircleError,
-  ICircleSuccess,
-  IDownload,
-  IExclamation,
-  IEyeClosed,
-  IEyeOpened,
-  IFile,
-  IFilter,
-  IFilterActive,
-  IHintSuccess,
-  IInfo,
-  IList,
-  IMail,
-  IMinus,
-  IPlus,
-  IPrinter,
-  IRefresh,
-  ISearch,
-  ISortArrow,
-  ITrash,
-  ITriplet,
-  IX,
+  ICalendar24,
+  IChevron24,
+  ICircleEmpty24,
+  ICircleError24,
+  ICircleSuccess24,
+  IDownload24,
+  IExclamation24,
+  IEyeClosed24,
+  IEyeOpened24,
+  IFile24,
+  IFilter24,
+  IFilterActive24,
+  IHintSuccess24,
+  IInfo24,
+  IList24,
+  IMail24,
+  IMinus24,
+  IPlus24,
+  IPrinter24,
+  IRefresh24,
+  ISearch24,
+  ISortArrow24,
+  ITrash24,
+  ITriplet24,
+  IX24,
 }
