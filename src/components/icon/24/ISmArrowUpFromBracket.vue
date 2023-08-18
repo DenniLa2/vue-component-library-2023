ea3edda8d78a07b03eedf268accd472ca2191e14
KukhtaSvetlana <kuxta.svetlana97@gmail.com>
2023-08-18 13:05:57 +0300

    UP-913: add icon for email to send

---
+++ src/components/icon/24/ISmArrowUpFromBracket.vue
@@ -0,0 +1,41 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M6 14V16C6 17.6569 7.34315 19 9 19H15C16.6569 19 18 17.6569 18 16V14"
+      class="stroke-primary"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+
+    <path
+      d="M12 14V4"
+      class="stroke-primary"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+
+    <path
+      d="M9.17188 6.41408L12.0003 3.58569L14.8287 6.41412"
+      class="stroke-primary"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+
+<style scoped lang="scss">
+.stroke-primary {
+  fill: var(--icon-primary)
+}
+
+</style>
