b6b223c1f6412e1b927e1a4345bcda697c76c9e6
KukhtaSvetlana <kuxta.svetlana97@gmail.com>
2023-09-01 09:13:20 +0300

    UP-1126: add icon Exit

---
+++ src/components/icon/bg/IBgExit.vue
@@ -0,0 +1,37 @@
+<template>
+  <svg
+    width="96"
+    height="96"
+    viewBox="0 0 96 96"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      width="96"
+      height="96"
+      rx="20"
+      class="bg-fill"
+    />
+
+    <path
+      d="M46 48H65M65 48L62 44M65 48L62 52M54 56V58C54 61.3137 51.3137 64 48 64H40C36.6863 64 34 61.3137 34 58V38C34 34.6863 36.6863 32 40 32H48C51.3137 32 54 34.6863 54 38V40"
+      class="icon-stroke"
+      stroke-width="4"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+
+<style scoped lang="scss">
+
+.bg-fill {
+  fill: var(--ui-selected);
+}
+
+.icon-stroke {
+  stroke: var(--icon-primary);
+}
+
+</style>
